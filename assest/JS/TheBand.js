9d26a807ae2f21c1eb7f8ec86e0873ab4d8897b5
hungdo161 <dongochung.161@gmail.com>
2023-05-28 14:36:24 +0700

    responsive inprogress

--- assest/JS/TheBand.js
+++ assest/JS/TheBand.js
@@ -1,31 +1,76 @@
 
 
-const buyTecketBtns = document.querySelectorAll(".js-tourPlace_buyBtn")
-const getModal = document.querySelector(".js-modal")
-const modalBuyTicketSection = document.querySelector(".js-modal_buyTicketSection")
-const closeModals = document.querySelectorAll(".js-modal_closedBtn")
 
-function showModal() {
-    getModal.classList.add("open")
-};
+
+
+// 28/05/2023
+
+const buyBtns = document.querySelectorAll(".js-tourPlace_buyBtn") ; 
+const getModal = document.querySelector(".js-modal") ;
+const getCloseBtns = document.querySelectorAll(".js-modal_closedBtn") ; 
+
+const getModalContainer = document.querySelector(".js-modal_buyTicketSection")
+
+
+function showModal () {
+    getModal.classList.add("open");
+}
+
+for (const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showModal)
+}
+
 
 function hideModal () {
-    getModal.classList.remove("open")
-}
+    getModal.classList.remove("open");
+};
 
-
-for (const buyTecketBtn of buyTecketBtns) {
-    buyTecketBtn.addEventListener('click', showModal)
+for (const closeBtn of getCloseBtns) {
+    closeBtn.addEventListener("click",hideModal)
 };
 
 
-for (const closeModal of closeModals) {
-    closeModal.addEventListener('click', hideModal)
-}
+getModal.addEventListener('click', hideModal);
 
-getModal.addEventListener('click', hideModal)
+// Ngăn chặn sự nổi bọt(Propagation)
 
-// ngăn chặn sử nổi bọt 
-modalBuyTicketSection.addEventListener('click', function (event) {
+getModalContainer.addEventListener("click", function (event) {
     event.stopPropagation()
-})
+}
+);
+
+
+
+
+
+// 27/05/2023
+
+// const buyTecketBtns = document.querySelectorAll(".js-tourPlace_buyBtn")
+// const getModal = document.querySelector(".js-modal")
+// const modalBuyTicketSection = document.querySelector(".js-modal_buyTicketSection")
+// const closeModals = document.querySelectorAll(".js-modal_closedBtn")
+
+// function showModal() {
+//     getModal.classList.add("open")
+// };
+
+// function hideModal () {
+//     getModal.classList.remove("open")
+// }
+
+
+// for (const buyTecketBtn of buyTecketBtns) {
+//     buyTecketBtn.addEventListener('click', showModal)
+// };
+
+
+// for (const closeModal of closeModals) {
+//     closeModal.addEventListener('click', hideModal)
+// }
+
+// getModal.addEventListener('click', hideModal)
+
+// // ngăn chặn sử nổi bọt 
+// modalBuyTicketSection.addEventListener('click', function (event) {
+//     event.stopPropagation()
+// })
